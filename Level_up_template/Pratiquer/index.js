d6366fe60797b10dec42111a61b2aa75b77b456f
Mathilde-J <72550752+Mathilde-J@users.noreply.github.com>
2021-02-16 16:15:59 +0100

    reecriture de code "pratiquer"

---
+++ Level_up_template/Pratiquer/index.js
@@ -0,0 +1,22 @@
+// -------------------------------EXO DE VIBE REECRITURE DE CODE----------------------------
+
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+// function reqListener () {
+//   console.log(this.responseText);
+// }
+
+// var req = new XMLHttpRequest();
+// req.responseType = 'json';
+// req.open('GET', '<https://www.affirmations.dev/>', true);
+// req.onload  = reqListener;
+// req.send(null);
+
+// ------------------------------------NEW ECRITURE------------------------------
+
+
+const fetch = require('node-fetch');
+
+fetch('https://www.affirmations.dev')
+    .then(res => res.text())
+    .then(text => console.log(text))
